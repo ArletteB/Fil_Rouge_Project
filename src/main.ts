99afc1c0255d7f9eee917b62f20eb5d19e843077
ArletteB <adiath-arlette.bello@campusdulac.com>
2023-04-02 19:23:37 +0200

    add info

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 const PORT = process.env.PORT || 8000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const corsOptions: CorsOptions = {
+    origin: [`${process.env.CLIENT_APP_URL}`],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE, OPTIONS',
+    allowedHeaders: 'Authorization, Content-Type',
+  };
+
+  app.enableCors(corsOptions);
   await app.listen(PORT);
 }
 bootstrap();
